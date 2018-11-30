12a3c72729fb31152fbe6de6636369b9005661c1
pol.sabadell <psabadell@outlook.com>
2018-11-30 14:42:11 +0100

    Pantalla de Inicio
    Aquí está lo basico de la pantalla de inicio, pero no está acabado, queda retocar ciertas cosas

--- app/src/main/java/com/example/luisrubiohernan/quizbrand/MainActivity.java
+++ app/src/main/java/com/example/luisrubiohernan/quizbrand/MainActivity.java
@@ -2,12 +2,36 @@ package com.example.luisrubiohernan.quizbrand;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.google.gson.Gson;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView title;
+    private ImageView subtitle;
+    private Gson gson;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        title = findViewById(R.id.title);
+        subtitle = findViewById(R.id.subtitle);
+
+        gson = new Gson();
+/*
+       Glide.with(this)
+                .load("file:///android_asset/fondo.jpg")
+                .into(title);
+
+        Glide.with(this)
+                .load("file:///android_asset/foto.jpg")
+                .apply(RequestOptions.circleCropTransform())
+                .into(subtitle);*/
     }
 }
