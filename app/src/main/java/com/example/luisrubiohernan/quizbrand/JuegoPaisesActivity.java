e777019630d87c5141f84892d9a2f9833f0bfba9
RaulDiMont <RaulDiMont@users.noreply.github.com>
2018-12-30 22:03:13 +0100

    Creado el .json de prueba, con 3 compañias incluidas. También incluidos los 3 logos correspondientes, todo ello en la carpeta "assets". Creado el layout para la versión del juego de países y sectores.

---
+++ app/src/main/java/com/example/luisrubiohernan/quizbrand/JuegoPaisesActivity.java
@@ -0,0 +1,14 @@
+package com.example.luisrubiohernan.quizbrand;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class JuegoPaisesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.juego_paises);
+    }
+}
