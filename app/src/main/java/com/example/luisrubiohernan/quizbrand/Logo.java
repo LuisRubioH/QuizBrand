1bb76c0c4ce3fd098e5c6839c4694895667bbf4f
RaulDiMont <RaulDiMont@users.noreply.github.com>
2019-01-01 23:16:10 +0100

    Creada la clase Logo con los atributos Name, Country, Sector e Image. Image es el path de el archivo de la imagen del logo

---
+++ app/src/main/java/com/example/luisrubiohernan/quizbrand/Logo.java
@@ -0,0 +1,21 @@
+package com.example.luisrubiohernan.quizbrand;
+
+public class Logo {
+    private String Name, Country, Sector, Image;
+
+    public String getName() {
+        return Name;
+    }
+
+    public String getCountry() {
+        return Country;
+    }
+
+    public String getSector() {
+        return Sector;
+    }
+
+    public String getImage() {
+        return Image;
+    }
+}
