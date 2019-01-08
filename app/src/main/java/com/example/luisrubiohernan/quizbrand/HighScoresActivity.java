8843aebe3fd05bf30faada80e76182b8bba888cf
raul.diaz.monterde <raul.monterdesc@gmail.com>
2019-01-08 22:04:35 +0100

    Creada clase puntuaciones,etc

--- app/src/main/java/com/example/luisrubiohernan/quizbrand/HighScoresActivity.java
+++ app/src/main/java/com/example/luisrubiohernan/quizbrand/HighScoresActivity.java
@@ -3,6 +3,7 @@ package com.example.luisrubiohernan.quizbrand;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 
 public class HighScoresActivity extends AppCompatActivity {
@@ -11,6 +12,13 @@ public class HighScoresActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_high_scores);
+
+        TextView puntuacionPaises = findViewById(R.id.textViewPuntPais);
+        TextView puntuacionSectores = findViewById(R.id.textViewPuntSect);
+
+        puntuacionPaises.setText(Puntuacion.getPuntuacionPaises());
+        puntuacionSectores.setText(Puntuacion.getPuntuacionSectores());
+
     }
 
 
